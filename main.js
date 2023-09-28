e47d3a35788af2d6eee80f64609a7f9699bb1def
Jaeyoung Yoon <disfenteria@gmail.com>
2023-09-28 16:21:19 -0700

    add images and changed html and css code / remove js file

--- main.js
+++
@@ -1,7 +0,0 @@
-        // Initialize Masonry layout
-        var grid = document.querySelector('.grid');
-        var masonry = new Masonry(grid, {
-            itemSelector: '.grid-item',
-            columnWidth: '.grid-item',
-            gutter: 20
-        });
